4910820b59efcf9a9438341c0ec111232e7737b8
jvandy83 <vanthedev@gmail.com>
2021-09-25 20:25:16 -0600

    added link to create url page

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 function App() {
-	return <div className='App'>Home</div>;
+	return (
+		<div className='App'>
+			<h1>Home Page</h1>
+			<div>
+				<Link to='/urls'>Create a new URL</Link>
+			</div>
+		</div>
+	);
 }
 
 export default App;
